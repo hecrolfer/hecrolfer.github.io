0c3fce5593ba72c3fab27837734f18d9f38af04c
Hector Roldan Fernandez <hroldan@mercadona.es>
2024-11-12 09:52:17 +0100

    feat: changing web

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 let pantallaActual = 0;
 const pantallas = document.querySelectorAll('.pantalla');
 
+// Función para avanzar a la siguiente pantalla
 function avanzarPantalla() {
-    // Oculta la pantalla actual
     pantallas[pantallaActual].classList.remove('visible');
-    
-    // Avanza a la siguiente pantalla
     pantallaActual++;
-    
-    // Muestra la siguiente pantalla si existe
-    if (pantallaActual < pantallas.length) {
+    if (pantallaActual < pantallas.length - 1) { // Hasta la pantalla final
         pantallas[pantallaActual].classList.add('visible');
     }
 }
 
-function iniciarMelodia() {
-    alert("¡Que empiece tu aventura musical!");
+// Función para mostrar la última frase después de abrir el regalo
+function mostrarUltimaFrase() {
+    document.getElementById('pantalla-final').classList.remove('visible');
+    document.getElementById('pantalla-final-revelacion').classList.add('visible');
 }
