76fa383dbab4718dc00836d3de510154a69a813c
Hector Roldan Fernandez <hroldan@mercadona.es>
2024-11-13 12:13:52 +0100

    feat: changing text

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let pantallaActual = 0;
 const pantallas = document.querySelectorAll('.pantalla');
 
-// Función para avanzar a la siguiente pantalla
 function avanzarPantalla() {
     pantallas[pantallaActual].classList.remove('visible');
     pantallaActual++;
@@ -10,13 +9,11 @@ function avanzarPantalla() {
     }
 }
 
-// Función para descifrar el texto de una carta
 function descifrarTexto(cartaId, fraseDescifrada) {
     let carta = document.getElementById(cartaId);
     carta.innerText = fraseDescifrada;
 }
 
-// Función para mostrar la última frase después de abrir el regalo
 function mostrarUltimaFrase() {
     document.getElementById('pantalla-final').classList.remove('visible');
     document.getElementById('pantalla-final-revelacion').classList.add('visible');
